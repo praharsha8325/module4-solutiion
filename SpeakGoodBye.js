c8adede54d24de40540c486609263842b5ab1464
praharsha8325 <60357629+praharsha8325@users.noreply.github.com>
2020-04-30 21:10:59 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,8 +1,8 @@
 (function(window) {
     var byeSpeaker = new Object();
-    var speakWord = "Good Bye";
+   // var speakWord = "Good Bye";
     byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
+        console.log("Good Bye " + name);
     };
     window.byeSpeaker = byeSpeaker;
 })(window);
